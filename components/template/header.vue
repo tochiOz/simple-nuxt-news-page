ebf7aa647d0f07c28b49d8b527b54c5e0d896b2d
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-11 18:37:24 +0100

    Githuib code link added

--- components/template/header.vue
+++ components/template/header.vue
@@ -4,7 +4,9 @@
       <b-navbar-brand href="#">Nigeria News</b-navbar-brand>
 
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="">GitHub Code</b-nav-item>
+        <b-nav-item href="https://github.com/Tueloper/simple-nuxt-news-page"
+          >GitHub Code</b-nav-item
+        >
       </b-navbar-nav>
     </b-navbar>
   </div>
