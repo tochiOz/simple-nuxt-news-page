90b15a57f9bceb9ff939136c781cd9d66f30fd1c
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-11 18:44:22 +0100

    Githuib code link added

--- components/template/header.vue
+++ components/template/header.vue
@@ -4,7 +4,9 @@
       <b-navbar-brand href="#">Nigeria News</b-navbar-brand>
 
       <b-navbar-nav class="ml-auto">
-        <b-nav-item href="https://github.com/Tueloper/simple-nuxt-news-page"
+        <b-nav-item
+          href="https://github.com/Tueloper/simple-nuxt-news-page"
+          target="_blank"
           >GitHub Code</b-nav-item
         >
       </b-navbar-nav>
--- store/index.js
+++ store/index.js
@@ -14,7 +14,7 @@ export const mutations = {
 export const actions = {
 	async loadNews ({ commit }) {
 		await axios
-			.get('http://newsapi.org/v2/top-headlines?country=ng&apiKey=d9b8dc0310184d419e7df9c70e4b9a7f')
+			.get('https://newsapi.org/v2/top-headlines?country=ng&apiKey=d9b8dc0310184d419e7df9c70e4b9a7f')
 			.then(async (res) => {
 				const news = res.data.articles;
 
