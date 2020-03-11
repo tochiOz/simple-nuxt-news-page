88eff63ed9f007483ef062a5fd48d548531f2fb6
Tueloper <ozurumbatochukwu@yahoo.com>
2020-03-11 18:57:09 +0100

    Footer added

--- components/template/footer.vue
+++ components/template/footer.vue
@@ -1,3 +1,33 @@
 <template>
-  <div>Footer</div>
+  <div>
+    <div class=" bg-danger footer">
+      Made With Love ♡ by
+      <a href="https://github.com/Tueloper">Tochukwu Ozurumba </a> &copy;
+      {{ this.year }}
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+<style scoped>
+.footer {
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
